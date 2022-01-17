2df839569db4a27729d5b382b218c48230d36a4e
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2022-01-17 20:56:35 +0530

    Add files via upload

---
+++ Arithmetic.js
@@ -0,0 +1,31 @@
+var a=7;
+var b=2;
+var result=a+b;
+console.log(result);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+var result=a%b;
+console.log(result);
+a++;
+console.log(a);
+b--;
+console.log(b);
+++a;
+console.log(a);
+--b;
+console.log(b);
+
+//postfix//
+var c=7;
+var result=c++ + 1;
+console.log(result);
+console.log(c);
+
+//prefix//
+var result=++c + 1;
+console.log(result);
+ 
+//concadetation//
+var lang="hii";
+console.log(lang + " kowsi" + 123);
