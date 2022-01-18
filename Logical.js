86691236dba618309ba47e1c0be6ec3e82354203
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2022-01-18 21:10:54 +0530

    Add files via upload

---
+++ Logical.js
@@ -0,0 +1,14 @@
+let voteeligible=true;
+let voterequired=false;
+ 
+//!(NOT)
+ console.log(!voteeligible);
+ console.log(!voterequired);
+
+ //||(OR)
+
+ console.log(voteeligible||voterequired);
+
+ //&&(AND)
+
+ console.log(voteeligible&&voterequired);
