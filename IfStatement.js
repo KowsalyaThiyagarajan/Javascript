d523bc91ce094fde986bd5be6161cb298d7c770b
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2022-01-18 21:10:12 +0530

    Add files via upload

---
+++ IfStatement.js
@@ -0,0 +1,10 @@
+let hours=new Date().getHours
+if(hours<=0 && hours<=12){
+    console.log("GOOD MORNING!!");
+    }
+    else if(hours<=12 && hours<=17){
+        console.log("GOOD AFTERNOON!!")
+    }
+    else{
+        console.log("GOOD EVENING!!")
+    }
