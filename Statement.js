87ceaddb7f7250472dcaa1118727f485831f646e
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2022-01-18 21:09:11 +0530

    Add files via upload

---
+++ Statement.js
@@ -0,0 +1,13 @@
+let Mark =80;
+if(Mark<99 || Mark==99){
+    console.log("A_grade");
+}
+else if(Mark<65 || Mark==65){
+    console.log("B_grade");
+}
+else if(Mark<45 || Mark==45){
+    console.log("C_grade");
+}
+else{
+    console.log("FAIL");
+}
