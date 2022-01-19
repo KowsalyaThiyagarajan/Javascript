eb482bb21a058b35c9d085c2bfa804b7c61c35a1
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2022-01-19 20:45:27 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+function Arithmetic(Num1, Num2) {
+    let add = Num1 + Num2;
+    let sub = Num1 - Num2;
+    let mul = Num1 * Num2;
+    let Div = Num1 / Num2;
+    let Mod = Num1 % Num2;
+    console.log("Addition:",add);
+    console.log("Subtraction:",sub);
+    console.log("Multiplication:",mul);
+    console.log("Division:",Div);
+    console.log("Modulus:",Mod);
+}
+Arithmetic(6, 2)
+
+function car(model,color){
+    let cars="Car model name is"+ " " +model +" "+ "and color is" +" "+ color;
+    console.log(cars);
+}
+car("volkswagen","black")
+car("Audi","Red")
